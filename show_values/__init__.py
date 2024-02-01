fadbb067801a5c06d8594c0545f656b3b3d49d10
ikatkov <ikatkov@gmail.com>
2024-02-01 13:27:38 -0800

    first commit

---
+++ show_values/__init__.py
@@ -0,0 +1,2 @@
+from .show_values_action import ShowValuesAction  # Note the relative import!
+ShowValuesAction().register()  # Instantiate and register to Pcbnew
---
+++ show_values/show_values_action.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import pcbnew
+import os
+import wx
+
+
+class ShowValuesAction(pcbnew.ActionPlugin):
+    def defaults(self):
+        self.name = "Show Values"
+        self.category = "Cleanup"
+        self.description = "Hide the reference Designators on the silkscreen, show values, if designator is hidden - reverse"
+        self.icon_file_name = os.path.join(
+            os.path.dirname(__file__), "./resources/show-value.png")
+        self.show_toolbar_button = True
+
+    def Run(self):
+        # Filter just selected footprints
+        selected_footprints: list[pcbnew.FOOTPRINT] = [
+            footprint for footprint in pcbnew.GetCurrentSelection()
+            if type(footprint).__name__ == 'FOOTPRINT'
+        ]
+
+        if len(selected_footprints) == 0:
+            # Show info dialog
+            dlg = wx.MessageDialog(None, "Please select one or multiple footprints!",
+                                   "No footprints selected", wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return
+
+        for footprint in selected_footprints:
+            reference = footprint.Reference()
+            if reference.IsVisible():
+                footprint.Value().SetLayer(reference.GetLayer())
+                reference.SetVisible(False)
+            else:
+                footprint.Value().SetLayer(pcbnew.B_Fab if footprint.IsFlipped() else pcbnew.F_Fab)
+                reference.SetVisible(True)
+
+        pcbnew.Refresh()
+        pcbnew.UpdateUserInterface()
